3cb938badc4dfd57bbc2894fdab375d9ce21d0b8
nikos kazas <nikoskazas3@gmail.com>
2026-03-08 20:27:34 +0200

    started creating routes

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
-# test
+from fastapi import FastAPI, UploadFile, File, Form, HTTPException
+from typing import Optional
+from pydantic import BaseModel
+from dotenv import load_dotenv
+import uuid
+import os
+from file_processor import process_file
+
+load_dotenv()
+
+medical = FastAPI()
+
+memory = {
+    "history": [],
+    "analysis": None,
+    "mode": "chat"
+} 
+
+@medical.post("/chat")
+def chat(msg: str = Form(default=""),mode: str = Form()):
+    memory["mode"] = mode 
+    if mode == "analysis":
+        
