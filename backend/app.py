e6405800890a88d088b119f4ab7235aa4bdf6035
johnTsab <187337489+johnTsab@users.noreply.github.com>
2026-03-10 00:00:00 +0200

    restructured the backend files and added some chat logic

--- backend/app.py
+++ backend/app.py
@@ -1,48 +1,13 @@
-from fastapi import FastAPI, UploadFile, File, Form, HTTPException
-from typing import Optional
-from pydantic import BaseModel
+from fastapi import FastAPI
+from routes.mediverifyRoutes import router
 from dotenv import load_dotenv
-import uuid
-import os
 from file_processor import process_file
 
 load_dotenv()
 
 medical = FastAPI()
+medical.include_router(router)
 
-memory = {
-    "history": [],
-    "analysis": None,
-    "mode": "chat"
-} 
-
-@medical.post("/chat")
-async def chat(msg: str = Form(default=""),mode: str = Form(),file:Optional[UploadFile]=File(None)):
-    memory["mode"] = mode 
-
-    #ANALYSIS MODE
-    if mode == "analysis":
-        if file:
-
-        elif msg:
-            if not memory["analysis"]:
-            
-            else:
-            
-        else:
-            reply = "Analysis Mode — ανέβασε ένα PDF ή εικόνα!"
-    
-    else: 
-        if not message:
-
-        else:
-
-    
-    return {
-        "reply": reply,
-        "mode":mode,
-        "analysis":memory.get("analysis")
-    }
 
         
     
---
+++ backend/routes/mediverifyRoutes.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+from controllers.authController import register, login, logout
+from controllers.chatController import chat
+from controllers.sessionController import get_session_route
+
+router = APIRouter()
+
+#AUTH 
+
+#CHAT 
+
+#SESSION
+
+#ROOT CHECKUP
